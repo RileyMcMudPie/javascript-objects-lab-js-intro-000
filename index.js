1cb20aebb5c7a29dc5ed13c0afa286bc18877bcf
Riley McMudPie <rydonamusic@gmail.com>
2020-07-06 19:49:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
-var newObj =  Object.assign({}, obj)
-return newObj
+return Object.assign({}, object, {[key]: value})
 }
